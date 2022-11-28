b5670462843c4e26e88fdf22d8ccef94389678b8
meisnate12 <meisnate12@gmail.com>
2022-11-28 14:39:11 -0500

    update arguments

--- auto_readme.py
+++ auto_readme.py
@@ -4,17 +4,33 @@ from urllib.parse import quote
 parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--name", dest="name", help="Name", type=str, default="")
 parser.add_argument("-s", "--style", dest="style", help="Style", type=str, default="")
-parser.add_argument("-d", "--directory", dest="directory", help="Directory", type=str, default="imagerepo")
+parser.add_argument("-d", "--directory", dest="directory", help="Directory", type=str, default="")
 args = parser.parse_args()
 
-repo = f"Plex Meta Manager People - {args.name}{f' ({args.style})' if args.style else ''}"
+names = {"bw": "Black & White"}
+
+if args.name:
+    name = args.name
+elif args.style:
+    name = names[args.style] if args.style in names else args.style.capitalize()
+else:
+    name = "Original"
+
+if args.directory:
+    directory = args.directory
+elif args.style:
+    directory = args.style
+else:
+    directory = "original"
+
+repo = f"Plex Meta Manager People - {name}{f' ({args.style})' if args.style else ''}"
 
 total_data = []
-letters = [l for l in os.listdir(args.directory) if l not in [".git", ".github", ".idea", "README.md"]]
+letters = [l for l in os.listdir(directory) if l not in [".git", ".github", ".idea", "README.md"]]
 letters.sort()
 total = 0
 for letter in letters:
-    images_folder = os.path.join(args.directory, letter, "Images")
+    images_folder = os.path.join(directory, letter, "Images")
     if not os.path.exists(images_folder):
         print(f"Images Folder: {images_folder} does not exist")
         continue
@@ -28,8 +44,8 @@ for letter in letters:
         total_data.extend(data)
         total_data.append("\n</details>")
         total += len(files)
-    with open(os.path.join(args.directory, letter, "README.md"), "w", encoding="utf-8") as f:
+    with open(os.path.join(directory, letter, "README.md"), "w", encoding="utf-8") as f:
         f.writelines([f"# {repo} - {letter} ({len(files)} Images)", "\n"] + data)
 
-with open(os.path.join(args.directory, "README.md"), "w", encoding="utf-8") as f:
+with open(os.path.join(directory, "README.md"), "w", encoding="utf-8") as f:
     f.writelines([f"# {repo} ({total} Images)", "\n"] + total_data)
